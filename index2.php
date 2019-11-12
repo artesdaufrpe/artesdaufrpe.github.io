7105e4e37c236ee19e0893beebbbee76e1a20237
jobsonrp <jobsonrp@gmail.com>
2019-11-12 12:44:36 -0300

    Alteração para o novo design: indexNew1.php

---
+++ index2.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include "html/head.html" ?>
+
+  <body>
+
+  <!-- Header geral do site -->
+  <?php include "html/header.html" ?>
+  
+  <div class="site-wrap">
+    
+    <div class="slide-one-item home-slider owl-carousel"> <!-- Início Carousel-->
+      
+      <div class="site-blocks-cover overlay" style="background-image: url(images/obra1/DSC3809_P.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+
+              <h2 class="text-white font-weight-light">Sem Título, 1999</h2>
+              <p class="mb-5"><b>Autor(a):</b> Christina Machado.</p>
+              <p><a href="obra1.php" class="btn btn-primary py-3 px-5 text-white">Visitar obra ></a></p>
+
+            </div>
+          </div>
+        </div>
+      </div>  
+
+      <div class="site-blocks-cover overlay" style="background-image: url(images/obra2/4_DSC3678.NEF_P.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+              <h2 class="text-white font-weight-light">90 anos dos cursos de ciências agrárias da UFRPE</h2>
+              <p class="mb-5"><b>Autor(a):</b> Corbiniano Lins</p>
+              <p><a href="obra2.php" class="btn btn-primary py-3 px-5 text-white">Visitar obra ></a></p>
+            </div>
+          </div>
+        </div>
+      </div>  
+
+      <div class="site-blocks-cover overlay" style="background-image: url(images/obra3/obra3_p.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+          <div class="row align-items-center justify-content-center text-center">
+
+            <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+              <h2 class="text-white font-weight-light">Busto do Prof. Francisco de Oliveira Magalhães, 2010</h2>
+              <p class="mb-5"><b>Autor(a):</b> Laércio Dias Braga</p>
+              <p><a href="obra3.php" class="btn btn-primary py-3 px-5 text-white">Visitar obra ></a></p>
+            </div>
+          </div>
+        </div>
+      </div>  
+
+    </div> <!-- Fim Carousel-->
+
+    <div class="site-section">
+      
+      <div class="container overlap-section">
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <a href="esculturas.php" class="unit-1 text-center">
+              <img src="images/galeria/obra7.jpg" alt="Image" class="img-fluid">
+              <div class="unit-1-text">
+                <h3 class="unit-1-heading">Esculturas</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <a href="pinturas.php" class="unit-1 text-center">
+              <img src="images/galeria/obra10.jpg" alt="Image" class="img-fluid">
+              <div class="unit-1-text">
+                <h3 class="unit-1-heading">Pinturas</h3>
+              </div>
+            </a>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <a href="talhas.php" class="unit-1 text-center">
+              <img src="images/galeria/obra9.jpg" alt="Image" class="img-fluid">
+              <div class="unit-1-text">
+                <h3 class="unit-1-heading">Talhas</h3>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary"><img src="images/arte_peq.png"></span></div>
+              <div>
+                <h3>Texto 1</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+                <p><a href="#">Leia mais</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary"><img src="images/arte_peq.png"></span></div>
+              <div>
+                <h3>Texto 2</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+                <p><a href="#">Leia mais</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="unit-4 d-flex">
+              <div class="unit-4-icon mr-4"><span class="text-primary"><img src="images/arte_peq.png"></span></div>
+              <div>
+                <h3>Texto 3</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quis molestiae vitae eligendi at.</p>
+                <p><a href="#">Leia mais</a></p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+      
+    <div class="site-blocks-cover overlay inner-page-cover" style="background-image: url(images/hero_bg_2.jpg); background-attachment: fixed;">
+      <div class="container">
+        <div class="row align-items-center justify-content-center text-center">
+
+          <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
+            <a href="#" class="play-single-big mb-4 d-inline-block popup-vimeo"><span class="icon-play"></span></a>
+            <h2 class="text-white font-weight-light mb-5 h1">Visualizar o vídeo sobre a obra</h2>
+            
+          </div>
+        </div>
+      </div>
+    </div>  
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center">
+            <h2 class="font-weight-light text-black">Veja mais</h2>
+            <p class="color-black-opacity-5">Acompanhe nosso projetos</p>
+          </div>
+        </div>
+        <div class="row mb-3 align-items-stretch">
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_1.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">Obras</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div> 
+          </div>
+          <div class="col-md-6 col-lg-6 mb-4 mb-lg-4">
+            <div class="h-entry">
+              <img src="images/hero_bg_2.jpg" alt="Image" class="img-fluid">
+              <h2 class="font-size-regular"><a href="#">Artes</a></h2>
+              <div class="meta mb-4">by Theresa Winston <span class="mx-2">&bullet;</span> Jan 18, 2019 at 2:00 pm <span class="mx-2">&bullet;</span> <a href="#">News</a></div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eligendi nobis ea maiores sapiente veritatis reprehenderit suscipit quaerat rerum voluptatibus a eius.</p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <a href="#" class="btn btn-outline-primary border-2 py-3 px-5">Veja mais</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="site-section border-top">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-12">
+            <h2 class="mb-5 text-black">Tour virtual de visitação às obras de artes visuais - murais, 
+              painéis e esculturas - do campus de Dois Irmãos da Universidade Federal Rural de Pernambuco
+               - UFRPE.</h2>
+            <p class="mb-0"><a href="https://tourbuilder.withgoogle.com/builder#play/ahJzfmd3ZWItdG91cmJ1aWxkZXJyEQsSBFRvdXIYgICgo9OwkQkM" class="btn btn-primary py-3 px-5 text-white">Nosso Google Tour</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    <?php include "html/footer.html" ?>
+
+  </div>
+
+  <?php include "html/scripts.html" ?>
+   
+  </body>
+</html>
