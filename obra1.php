766e2ba92f119ece3dc0d34a075bfcce5e7c77e3
jobsonrp <jobsonrp@gmail.com>
2019-11-30 12:29:20 -0300

    Ajuste no corpo (texto) da obra1.php finalizado, que será a referência para todas as outras obras.

--- obra1.php
+++ obra1.php
@@ -28,100 +28,95 @@
     <div class="site-wrap">
 
         <div class="site-section">
-            <div class="container">
-                <div class="row align-items-stretch">
-                    <div>
-                        <div class="col-sm-12 d-flex">
-                            <div class="unit-4-icon mr-4"><span class="text-primary"><img
-                                        src="images/arte_peq.png"></span></div>
-                            <div>
-                                <h3>Ficha Técnica</h3>
-                                <p>
-                                    <b>Título:</b> Sem Título <br>
-                                    <b>Autor(a):</b> Christina Machado <br>
-                                    <b>Técnica:</b> Mista <br>
-                                    <b>Materiais:</b> Cerâmica e pigmentos <br>
-                                    <b>Dimensão:</b> 25,60m (largura) x 2,4m (altura) <br>
-                                    <b>Data:</b> 1999 <br>
-                                    <b>Localização:</b> Subsolo do prédio CEGOE <br>
-                                </p>
-                                <br>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="col-sm-12 d-flex">
-                            <div class="unit-4-icon mr-4"><span class="text-primary"><img
-                                        src="images/arte_peq.png"></span></div>
-                            <div>
-                                <h3>Sobre o Autor</h3>
-                                <p>
-                                    <b>Biografia</b> <br>
 
-                                    1957, Belém - PA <br>
-                                    <br>
-                                    Radicada em Recife (desde 1959), é artista plástica e ceramista desde 1978, quando
-                                    iniciou sua carreira em ateliês de
-                                    desenho e pintura. Graduada em educação artística pela Universidade Federal de
-                                    Pernambuco, conheceu a cerâmica no início
-                                    dos anos 80 e passou a dedicar-se ao desenvolvimento de técnicas de modelagem,
-                                    desenho e pintura próprios à esta matéria.
-                                    Voltado ao desenvolvimento de uma concepção da cerâmica enquanto arte, o trabalho de
-                                    Christina Machado foi inicialmente
-                                    marcado pela criação de objetos, esculturas e painéis para interiores e exteriores,
-                                    onde a cerâmica integrava-se a projetos
-                                    arquitetônicos. <br>
-                                    Nos últimos cinco anos, seu trabalho tem se voltado a uma concepção da argila não
-                                    apenas como suporte sobre o qual pintar
-                                    ou modelar, mas como a obra em si, enfatizando sua natureza intrínseca. Assim, a
-                                    artista vem desenvolvendo técnicas para
-                                    o emprego da argila (com origens, texturas e tonalidades diversas) como cobertura ou
-                                    a própria tinta a ser utilizada,
-                                    fazendo com que a argila participe como elemento essencial de todos os estágios de
-                                    composição da obra. <br>
-                                    <br>
-                                    <b>Fonte:</b> Biografia disponibilizada por Christina Machado
-
-                                    <p><b>Disponível em: </b><a
-                                            href="http://www.galeriaestacao.com.br/acervo_artista/55" target="_blank"
-                                            class="btn btn-orange hover-black py-2 px-3">
-                                            galeriaestacao<br />
-                                        </a></p>
-
-                                </p>
-                                <br>
-                            </div>
+            <section class="main-section alabaster">
+                <!--main-section alabaster-start-->
+                <div class="container">
+                    <div class="row">
+                        <figure class="col-lg-1 col-sm-1 wow fadeInLeft">
+                            <img src="images/arte_peq.png" alt="">
+                        </figure>
+                        <div class="col-lg-11 col-sm-10 featured-work">
+                            <h2>Ficha Técnica</h2>
+                            <p>
+                                <b>Título:</b> Sem Título <br>
+                                <b>Autor(a):</b> Christina Machado <br>
+                                <b>Técnica:</b> Mista <br>
+                                <b>Materiais:</b> Cerâmica e pigmentos <br>
+                                <b>Dimensão:</b> 25,60m (largura) x 2,4m (altura) <br>
+                                <b>Data:</b> 1999 <br>
+                                <b>Localização:</b> Subsolo do prédio CEGOE <br>
+                            </p>
                         </div>
                     </div>
 
-                    <div>
-                        <div class="unit-4 d-flex">
-                            <div class="unit-4-icon mr-4"><span class="text-primary"><img
-                                        src="images/arte_peq.png"></span></div>
-                            <div>
-                                <h3>Sobre a Obra</h3>
-                                <p>
-                                    A obra se encontra no salão térreo ao lado do pátio do auditório/CEGOE.
-                                </p>
+                    <div class="row">
+                        <figure class="col-lg-1 col-sm-1 wow fadeInLeft">
+                            <img src="images/arte_peq.png" alt="">
+                        </figure>
+                        <div class="col-lg-11 col-sm-10 featured-work">
+                            <h2>Sobre o Autor</h2>
+                            <p>
+                                <b>Biografia</b> <br>
+
+                                1957, Belém - PA <br>
                                 <br>
-                            </div>
+                                Radicada em Recife (desde 1959), é artista plástica e ceramista desde 1978, quando
+                                iniciou sua carreira em ateliês de
+                                desenho e pintura. Graduada em educação artística pela Universidade Federal de
+                                Pernambuco, conheceu a cerâmica no início
+                                dos anos 80 e passou a dedicar-se ao desenvolvimento de técnicas de modelagem,
+                                desenho e pintura próprios à esta matéria.
+                                Voltado ao desenvolvimento de uma concepção da cerâmica enquanto arte, o trabalho de
+                                Christina Machado foi inicialmente
+                                marcado pela criação de objetos, esculturas e painéis para interiores e exteriores,
+                                onde a cerâmica integrava-se a projetos
+                                arquitetônicos. <br>
+                                Nos últimos cinco anos, seu trabalho tem se voltado a uma concepção da argila não
+                                apenas como suporte sobre o qual pintar
+                                ou modelar, mas como a obra em si, enfatizando sua natureza intrínseca. Assim, a
+                                artista vem desenvolvendo técnicas para
+                                o emprego da argila (com origens, texturas e tonalidades diversas) como cobertura ou
+                                a própria tinta a ser utilizada,
+                                fazendo com que a argila participe como elemento essencial de todos os estágios de
+                                composição da obra. <br>
+                                <br>
+                                <b>Fonte:</b> Biografia disponibilizada por Christina Machado
+
+                                <p><b>Disponível em: </b><a href="http://www.galeriaestacao.com.br/acervo_artista/55"
+                                        target="_blank" class="btn btn-orange hover-black py-2 px-3">
+                                        galeriaestacao<br />
+                                    </a></p>
+
+                            </p>
                         </div>
                     </div>
 
-                    <div class="container">
-
-                        <h3><br>Texto a ser definido</a></h3>
-
-                        <p>Texto a ser definido</p>
-
-                        <?php include "teste4.html" ?>
-
+                    <div class="row">
+                        <figure class="col-lg-1 col-sm-1 wow fadeInLeft">
+                            <img src="images/arte_peq.png" alt="">
+                        </figure>
+                        <div class="col-lg-11 col-sm-10 featured-work">
+                            <h2>Sobre a Obra</h2>
+                            <p>
+                                A obra se encontra no salão térreo ao lado do pátio do auditório/CEGOE.
+                            </p>
+                        </div>
                     </div>
-
-
 
                 </div>
-            </div>
+
+                <div class="container titleModal">
+
+                    <h3><br>Texto a ser definido</a></h3>
+
+                    <p>Texto a ser definido</p>
+
+                    <?php include "teste4.html" ?>
+
+                </div>
+            </section>
+
             </br></br></br>
             <div class="site-section backorange">
                 <div class="container">
@@ -132,10 +127,6 @@
             </div>
 
         </div>
-
-
-
-
 
         <?php include "html/footer.html" ?>
 
