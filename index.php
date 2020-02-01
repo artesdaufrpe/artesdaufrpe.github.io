06ad58c8fa1c70bc11177e943de3027d402b717c
jobsonrp <jobsonrp@gmail.com>
2020-02-01 16:59:08 -0300

    Ajustes na seção "Principais Artistas" do index.php

--- index.php
+++ index.php
@@ -1,162 +1,201 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <?php include "html/head.html" ?>
+<?php include "html/head.html" ?>
 
-  <body>
-  <!-- Header geral do site -->
-  <?php include "html/header.html" ?>
+<body>
+    <!-- Header geral do site -->
+    <?php include "html/header.html" ?>
 
-  <div class="site-wrap">
-    
-    <div class="slide-one-item home-slider owl-carousel"> <!-- Início Carousel-->
-      
-      <div class="site-blocks-cover overlay" style="background-image: url(images/obra1/imageP/1p.png);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
+    <div class="site-wrap">
 
-            <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+        <div class="slide-one-item home-slider owl-carousel">
+            <!-- Início Carousel-->
 
-              <h3 class="text-white font-weight-light">Sem Título, 1999</h3>
-              <p class="mb-5 text-orange"><b>Autora:</b> Christina Machado.</p>
-              <p><a href="obra1.php" class="btn btn-orange py-3 px-5 text-white">Visitar obra ></a></p>
+            <div class="site-blocks-cover overlay" style="background-image: url(images/obra1/imageP/1p.png);"
+                data-aos="fade" data-stellar-background-ratio="0.5">
+                <div class="container">
+                    <div class="row align-items-center justify-content-center text-center">
+
+                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+
+                            <h3 class="text-white font-weight-light">Sem Título, 1999</h3>
+                            <p class="mb-5 text-orange"><b>Autora:</b> Christina Machado.</p>
+                            <p><a href="obra1.php" class="btn btn-orange py-3 px-5 text-white">Visitar obra ></a></p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="site-blocks-cover overlay" style="background-image: url(images/obra2/imageP/1p.png);"
+                data-aos="fade" data-stellar-background-ratio="0.5">
+                <div class="container">
+                    <div class="row align-items-center justify-content-center text-center">
+
+                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+                            <h3 class="text-white font-weight-light">90 anos dos cursos de ciências agrárias da UFRPE
+                            </h3>
+                            <p class="mb-5 text-orange"><b>Autor:</b> Corbiniano Lins</p>
+                            <p><a href="obra2.php" class="btn btn-orange py-3 px-5 text-white">Visitar obra ></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="site-blocks-cover overlay" style="background-image: url(images/obra3/imageP/3p.png);"
+                data-aos="fade" data-stellar-background-ratio="0.5">
+                <div class="container">
+                    <div class="row align-items-center justify-content-center text-center">
+
+                        <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
+                            <h3 class="text-white font-weight-light">Busto do Prof. Francisco O. Magalhães, 2010</h3>
+                            <p class="mb-5 text-orange"><b>Autor:</b> Laércio Dias Braga</p>
+                            <p><a href="obra3.php" class="btn btn-orange py-3 px-5 text-white">Visitar obra ></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div> <!-- Fim Carousel-->
+
+        <div class="site-section backgreen">
+
+            <div class="container overlap-section">
+                <div class="row">
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <a href="esculturas.php" class="unit-1 text-center">
+                            <img src="images/galeria/obra7.jpg" alt="Image" class="img-fluid">
+                            <div class="unit-1-text">
+                                <h3 class="unit-1-heading">Esculturas</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <a href="pinturas.php" class="unit-1 text-center">
+                            <img src="images/galeria/obra10.jpg" alt="Image" class="img-fluid">
+                            <div class="unit-1-text">
+                                <h3 class="unit-1-heading">Pinturas</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <a href="talhas.php" class="unit-1 text-center">
+                            <img src="images/galeria/obra9.jpg" alt="Image" class="img-fluid">
+                            <div class="unit-1-text">
+                                <h3 class="unit-1-heading">Talhas</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="site-section backgreen">
+            <div class="container">
+                <h1 class="text-white-grey text-center h1"> Principais Artistas </h1><br />
+                <div class="row">
+
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div>
+                            <div class="row row-art-principal">
+                                <div class="col unit-4-icon mr-4">
+                                    <span class="text-primary"><img src="images/foto3x4.jpg"></span>
+                                </div>
+                                <div class="col text-white-grey div-text-bottom">
+                                    <div><span><h5>Corbiniano Lins</h5></span></div>
+                                </div>
+                            </div>
+
+                            <div class="text-white-grey">
+                                <p>É o artista com mais obras espalhadas pela universidade, sendo 13 no total incluindo
+                                    7 esculturas e 6 placas de conclusão de curso. As suas obras na Universidade têm como tema os
+                                    elementos que compõem o brasão da instituição, a agricultura e o trabalhador rural.</p>
+                                <div class="bottom-leia-mais">
+                                  <a href="art-corbiniano.php" class="btn btn-orange py-1 px-3 text-white">Leia mais</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div>
+                            <div class="row row-art-principal">
+                                <div class="col unit-4-icon mr-4">
+                                    <span class="text-primary"><img src="images/foto3x4.jpg"></span>
+                                </div>
+                                <div class="col text-white-grey div-text-bottom">
+                                    <div><span><h5>Inalda Xavier</h5></span></div>
+                                </div>
+                            </div>
+
+                            <div class="text-white-grey">
+                                <p>Principal homenageada do projeto, a artista participou dos principais grupos e
+                                    movimentos da história da arte de Pernambuco.
+                                    Participou da Sociedade de Arte Moderna do Recife. Inalda ministrou disciplinas de
+                                    artes na UFRPE.</p>
+                                <div class="bottom-leia-mais">
+                                  <a href="art-inalda-xavier.php" class="btn btn-orange py-1 px-3 text-white">Leia mais</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+                        <div>
+                            <div class="row row-art-principal">
+                                <div class="col unit-4-icon mr-4">
+                                    <span class="text-primary"><img src="images/foto3x4.jpg"></span>
+                                </div>
+                                <div class="col text-white-grey div-text-bottom">
+                                    <div><span><h5>Christina Machado</h5></span></div>
+                                </div>
+                            </div>
+
+                            <div class="text-white-grey">
+                                <p>Licenciada em Artes Plásticas pela
+                                    Universidade Federal de Pernambuco. Na década de 1980, aproximou-se da
+                                    cerâmica, material que passou a experimentar intensamente, desenvolvendo
+                                    projetos arquitetônicos e obras públicas.</p>
+                                <div class="bottom-leia-mais">
+                                  <a href="art-christina-machado.php" class="btn btn-orange py-1 px-3 text-white">Leia mais</a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
 
             </div>
-          </div>
         </div>
-      </div>  
 
-      <div class="site-blocks-cover overlay" style="background-image: url(images/obra2/imageP/1p.png);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
+        <div class="site-blocks-cover overlay inner-page-cover backvideo"
+            style="background-image: url(images/obra9/imageP/2p.png); background-attachment: fixed;">
+            <div class="container">
+                <div class="row align-items-center justify-content-center text-center">
 
-            <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
-              <h3 class="text-white font-weight-light">90 anos dos cursos de ciências agrárias da UFRPE</h3>
-              <p class="mb-5 text-orange"><b>Autor:</b> Corbiniano Lins</p>
-              <p><a href="obra2.php" class="btn btn-orange py-3 px-5 text-white">Visitar obra ></a></p>
+                    <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
+                        <a href="#" class="play-single-big mb-4 d-inline-block popup-vimeo"><span
+                                class="icon-play"></span></a>
+                        <h2 class="text-white font-weight-light mb-5 h1">Vídeo sobre o Projeto<br />(Em Breve)</h2>
+
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>  
 
-      <div class="site-blocks-cover overlay" style="background-image: url(images/obra3/imageP/3p.png);" data-aos="fade" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center justify-content-center text-center">
+        <?php //include "html/vejamais.html" ?>
 
-            <div class="col-md-8" data-aos="fade-up" data-aos-delay="800">
-              <h3 class="text-white font-weight-light">Busto do Prof. Francisco O. Magalhães, 2010</h3>
-              <p class="mb-5 text-orange"><b>Autor:</b> Laércio Dias Braga</p>
-              <p><a href="obra3.php" class="btn btn-orange py-3 px-5 text-white">Visitar obra ></a></p>
-            </div>
-          </div>
-        </div>
-      </div>  
+        <?php include "html/tour_virtual.html" ?>
 
-    </div> <!-- Fim Carousel-->
+        <?php include "html/footer.html" ?>
 
-    <div class="site-section backgreen">
-      
-      <div class="container overlap-section">
-        <div class="row">
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <a href="esculturas.php" class="unit-1 text-center">
-              <img src="images/galeria/obra7.jpg" alt="Image" class="img-fluid">
-              <div class="unit-1-text">
-                <h3 class="unit-1-heading">Esculturas</h3>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <a href="pinturas.php" class="unit-1 text-center">
-              <img src="images/galeria/obra10.jpg" alt="Image" class="img-fluid">
-              <div class="unit-1-text">
-                <h3 class="unit-1-heading">Pinturas</h3>
-              </div>
-            </a>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <a href="talhas.php" class="unit-1 text-center">
-              <img src="images/galeria/obra9.jpg" alt="Image" class="img-fluid">
-              <div class="unit-1-text">
-                <h3 class="unit-1-heading">Talhas</h3>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
-    
     </div>
 
+    <?php include "html/scripts.html" ?>
 
-    <div class="site-section backgreen">
-      <div class="container">
-      <h1 class="text-white-grey text-center h1"> Principais Artistas </h1><br/>
-        <div class="row align-items-stretch">
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <div class="unit-4 d-flex">
-              <div class="unit-4-icon mr-4"><span class="text-primary"><img style="width:100px" src="images/LOGO_branca.png"></span></div>
-              <div class="text-white-grey">
-                <h3>Corbiniano Lins</h3>
-                <p>É o artista com mais obras espalhadas pela universidade, sendo 13 no total incluindo 7 esculturas 
-                  e 6 placas de conclusão de curso. As suas obras na Universidade têm como tema os elementos que compõem o
-                  brasão da instituição, a agricultura e o trabalhador rural.</p>
-                <p><a href="art-corbiniano.php" class="btn btn-orange py-1 px-3 text-white">Leia mais</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <div class="unit-4 d-flex">
-              <div class="unit-4-icon mr-4"><span class="text-primary"><img style="width:100px" src="images/LOGO_branca.png"></span></div>
-              <div class="text-white-grey">
-                <h3>Inalda Xavier</h3>
-                <p>Principal homenageada do projeto, a artista participou dos principais grupos e movimentos da história da arte de Pernambuco.
-Participou da Sociedade de Arte Moderna do Recife. Inalda ministrou disciplinas de artes na UFRPE.</p><br/>
-                <p><a href="art-inalda-xavier.php" class="btn btn-orange py-1 px-3 text-white">Leia mais</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-            <div class="unit-4 d-flex">
-              <div class="unit-4-icon mr-4"><span class="text-primary"><img style="width:100px" src="images/LOGO_branca.png"></span></div>
-              <div class="text-white-grey">
-                <h3>Christina Machado</h3>
-                <p>
-                Licenciada em Artes Plásticas pela
-Universidade Federal de Pernambuco. Na década de 1980, aproximou-se da
-cerâmica, material que passou a experimentar intensamente, desenvolvendo
-projetos arquitetônicos e obras públicas.
-                </p>
-                <p><a href="art-christina-machado.php" class="btn btn-orange py-1 px-3 text-white">Leia mais</a></p>
-              </div>
-            </div>
-          </div>
+</body>
 
-        </div>
-      </div>
-    </div>
-      
-    <div class="site-blocks-cover overlay inner-page-cover backvideo" style="background-image: url(images/obra9/imageP/2p.png); background-attachment: fixed;">
-      <div class="container">
-        <div class="row align-items-center justify-content-center text-center">
-
-          <div class="col-md-7" data-aos="fade-up" data-aos-delay="400">
-            <a href="#" class="play-single-big mb-4 d-inline-block popup-vimeo"><span class="icon-play"></span></a>
-            <h2 class="text-white font-weight-light mb-5 h1">Vídeo sobre o Projeto<br/>(Em Breve)</h2>
-            
-          </div>
-        </div>
-      </div>
-    </div>  
-
-    <?php //include "html/vejamais.html" ?> 
-    
-    <?php include "html/tour_virtual.html" ?>
-  
-    <?php include "html/footer.html" ?>
-
-    </div>
-
-  <?php include "html/scripts.html" ?>
-
-  </body>
 </html>
